09e5105d3184d878b809a13ad9e061a820d26b93
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-23 10:36:11 -0700

    Create registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,23 @@
+class RegistrationsController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :about, :password, :password_confirmation)
+  end
+
+end
