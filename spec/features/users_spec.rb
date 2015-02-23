22329dded2a7bc74ce32627a37db257c74f6d601
Caleb Atwood <calebwilliamatwood@gmail.com>
2015-02-23 13:26:27 -0700

    Test that Users can CRUD courses

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -27,4 +27,31 @@ describe 'users' do
     click_button "Sign In"
     expect(page).to have_content "Courses Index"
   end
+
+  it 'test user can CRUD courses' do
+    click_link "Sign In"
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+    fill_in "Username", with: "countchocula123"
+    fill_in "Password", with: "123456"
+    click_button "Sign In"
+    expect(page).to have_content "Courses Index"
+    click_link "Courses Index"
+    expect(page).to have_content "Add new course"
+    click_link "Add new course"
+    expect(page).to have_content "Title"
+    expect(page).to have_content "Day class?"
+    fill_in "Title", with: "Counting 101"
+    click_button "Create Course"
+    expect(page).to have_content "Counting 101"
+    click_link "Edit Course"
+    expect(page).to have_content "Edit this course"
+    fill_in "Title", with: "Counting 123"
+    click_button "Update Course"
+    expect(page).to have_content "Counting 123"
+    click_link "Delete Course"
+    expect(page).to have_content "Courses Index"
+  end
+
+
 end
